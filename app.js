8e552a2ada6f9eb72de20dba0f0efdbf15f42d15
JaveriyaSaleem <javeriyasaleem678@gmail.com>
2024-09-26 14:20:24 +0500

    added

---
+++ app.js
@@ -0,0 +1,8 @@
+var img = document.getElementById('imageDiv')
+document.getElementById('onBtn').addEventListener("click",function(){
+    img.src  = "image/bulb on.png"
+})
+
+document.getElementById('offBtn').addEventListener("click",function(){
+    img.src  = "image/Bulb off.png"
+})
